c55317b4f3a8327219aeeaabc5f63a109b52d44e
artemkulakov <artemkulakov250@gmail.com>
2023-07-13 11:32:29 +0300

    Init sync with bitrix vm

---
+++ .top_links.menu.php
@@ -0,0 +1,24 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Корпоративный портал", 
+		"/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Интернет сайт", 
+		"http://www.1c-bitrix.ru/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
